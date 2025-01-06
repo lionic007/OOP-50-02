4e15fc04f8d3649d0503352fea99cc2905d81f40
Sergei S <lionic007@gmail.com>
2025-01-06 18:26:32 +0600

    Feat: реализован класс Hero добавлены методы

---
+++ Homeworks/homework1.py
@@ -0,0 +1,15 @@
+class Hero:
+    def __init__(self, name, lvl, hp):
+        self.name = name
+        self.lvl = lvl
+        self.hp = hp
+
+    def introduce(self):
+        print(f"Привет, меня зовут {self.name}, мой lvl {self.lvl} , мое HP {self.hp}.")
+
+    def is_adult(self):
+        if self.lvl >= 10:
+            return True
+        else:
+            return False
+
