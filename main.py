fc6a9b8b8a3f8abb82bed6d59555d0eb2cb1cccd
Sergei S <lionic007@gmail.com>
2025-01-09 21:20:39 +0600

    refactor: закоментировано использование класса Hero

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from hw.homework1 import Hero
+from HomeWorks.homework1 import Hero
+from HomeWorks.homework2 import Archer
+
 
 def main():
+    # spider_man = Hero("Peter Parker", 11, 100)
+    #
+    # spider_man.introduce()
+    # print(str(spider_man))
 
-    spider_man = Hero("Peter Parker", 11, 100)
+    # Экземпляр класса Archer
+    lara_croft = Archer("Lara Croft", 1, 100, 5, 50, 'female')
 
-    spider_man.introduce()
-    print(str(spider_man))
+    lara_croft.attack()
+    lara_croft.rest()
+    print(f"{lara_croft.status()}")
+    lara_croft.setPrecision(90)
+    print(f"{lara_croft.status()}")
+
+    for i in range(1, 10):
+        lara_croft.setPrecision(i)
 
 
-
+# Точка входа приложения
 main()
